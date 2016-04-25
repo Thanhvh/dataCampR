158611c8d3b6a029742d44e7aac8e5095990fc93
Q4living <thomasqian@yahoo.com>
2016-04-25 21:55:55 +0800

    Additional code
    additional

--- Intermediate.r
+++ Intermediate.r
@@ -538,3 +538,37 @@ lapply(temp,max)
 
 # Use sapply() to find each day's maximum temperature
 sapply(temp,max)
+
+# temp is already defined in the workspace
+
+# Finish function definition of extremes_avg
+extremes_avg <- function(x) {
+  ( min(x) + max(x) ) / 2
+}
+
+# Apply extremes_avg() over temp using sapply()
+sapply(temp, extremes_avg)
+# 4.0 9.0 2.5 2.5 5.5 3.0 5.0
+
+# Apply extremes_avg() over temp using lapply()
+lapply(temp, extremes_avg)
+# [[1]]
+# [1] 4
+
+# [[2]]
+# [1] 9
+
+# [[3]]
+# [1] 2.5
+
+# [[4]]
+# [1] 2.5
+
+# [[5]]
+# [1] 5.5
+
+# [[6]]
+# [1] 3
+
+# [[7]]
+# [1] 5
