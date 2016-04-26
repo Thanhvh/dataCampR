960ce0784e6f4ae952fbe29ce023e12369e4f411
Q4living <thomasqian@yahoo.com>
2016-04-26 08:59:12 +0800

    update

--- Intermediate.r
+++ Intermediate.r
@@ -785,3 +785,48 @@ sapply(todos, sort)
 #
 #
 # Chapter 5 Utilities
+# abs(): calculate the absolute value.
+# sum(): calculate the sum of all the values in a data structure.
+# mean(): calculate the arithmetic mean.
+# round(): Round the values to 0 decimal places by default. 
+# seq(): Generate sequences, by specifying the from, to and by arguments.
+# rep(): Replicate elements of vectors and lists.
+# sort(): Sort a vector in ascending order. Works on numerics, but also on character strings and logicals.
+# rev(): Reverse the elements in a data structures for which reversal is defined.
+# str(): Display the structure of any R object.
+# append(): Merge vectors or lists.
+# is.*(): Check for the class of an R object.
+# as.*(): Convert an R object from one class to another.
+# unlist(): Flatten (possibly embedded) lists to produce a vector.
+
+# The errors vector has already been defined for you
+errors <- c(1.9, -2.6, 4.0, -9.5, -3.4, 7.3)
+
+# Sum of absolute rounded values of errors
+round(sum(abs(errors)), digits = 0)
+# 29
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- list(16, 9, 13, 5, 2, 17, 14)
+facebook <- list(17, 7, 5, 16, 8, 13, 14)
+
+# Convert linkedin and facebook to a vector: li_vec and fb_vec
+li_vec <- unlist(linkedin)
+fb_vec <- unlist(facebook)
+
+# Append fb_vec to li_vec: social_vec
+social_vec <- append(li_vec, fb_vec)
+
+# Sort social_vec
+sort(social_vec, decreasing = TRUE)
+# [1] 17 17 16 16 14 14 13 13  9  8  7  5  5  2
+
+# Create first sequence: seq1
+seq1 <- seq(1,500,3)
+
+# Create second sequence: seq2
+seq2 <- seq(1200,900,-7)
+
+# Calculate total sum of the sequences
+sum(seq1,seq2)
+# [1] 87029
