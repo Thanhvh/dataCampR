78ab0ca24a1b11a6a3810290c62524b84a1160bf
Thomas Qian <thomasq@yahoo-inc.com>
2016-05-02 21:06:23 +0800

    update to github

--- Cleaning Data.r
+++ Cleaning Data.r
@@ -415,6 +415,63 @@ weather6 <- mutate_each(weather5, funs(as.numeric), CloudCover:WindDirDegrees)
 # Look at result
 str(weather6)
 
+# Count missing values
+sum(is.na(weather6))
+# 6
+# Find missing values
+summary(weather6)
+
+# Find indices of NAs in Max.Gust.SpeedMPH
+ind <- which(is.na(weather6$Max.Gust.SpeedMPH))
+# [1] 169 185 251 275 316 338
+
+# Look at the full rows for records missing Max.Gust.SpeedMPH
+weather6[ind, ]
+
+# Review distributions for all variables
+summary(weather6)
+
+# Find row with Max.Humidity of 1000
+ind <- which(weather6$Max.Humidity == 1000)
+
+# Look at the data for that day
+weather6[ind, ]
+
+# Change 1000 to 100
+weather6$Max.Humidity[ind] <- 100
+
+# Look at summary of Mean.VisibilityMiles
+summary(weather6$Mean.VisibilityMiles)
+
+# Get index of row with -1 value
+ind <- which(weather6$Mean.VisibilityMiles == -1)
+
+# Look at full row
+weather6[ind,]
+
+# Set Mean.VisibilityMiles to the appropriate value
+weather6$Mean.VisibilityMiles[ind] <- 10
+
+# Clean up column names
+names(weather6) <- new_colnames
+
+# Replace empty cells in events column
+weather6$events[weather6$events == ""] <- "None"
+    
+# Print the first 6 rows of weather6
+head(weather6,6)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
